5490ff107abff80eba043d509ecab8d379433e75
Yohan Kim <yohankim03@gmail.com>
2023-05-10 10:05:28 +1000

    added myreg and tribuf

---
+++ my_reg.v
@@ -0,0 +1,13 @@
+module my_reg(x, enable, y, clk);
+	input clk, enable;
+	input [2:0] x;
+	output reg [2:0] y;
+	
+	always @ (posedge clk) begin
+		if (enable == 1'b1)
+			y = x;
+	end
+
+
+
+endmodule
---
+++ tri_buf.v
@@ -0,0 +1,13 @@
+module tri_buf (a,b,enable);
+	input[3:0] a;
+	output [3:0] reg b;
+	input enable;
+always @ (enable or a) begin
+	if (enable) begin
+		b = a;
+	end else begin
+		b =1’bz;
+	end
+end
+
+endmodule
